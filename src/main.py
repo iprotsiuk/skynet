e63dec752eb575620769abbe30a8529789b6dbdb
pavlop <poliakov.pavel@gmail.com>
2019-08-15 22:15:22 -0700

    Added timed directions

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from src import actor
 import time
+from src import debug_utils
 
 print("....")
 time.sleep(0.3)
@@ -13,15 +14,20 @@ print (" Satrted ")
 print ("!Satrted!")
 
 a = actor.Actor()
-
+a.update_plan()
 while True:
   time.sleep(3)
-  a.update_plan()
-  dirs = a.planned_directions
+
+  dirs = a.planned_directions_with_time
   print("taraget: ", a._cur_target_x, a._cur_target_y)
   print("target_square: ", a._cur_target_square)
-  print("new direction = ", dirs[0])
-  a.hero.move(dirs[0])
+  print("planned_directions_with_time = ", dirs)
+
+  debug_utils.draw_path_on_map(a.black_map, a.planned_pixel_path, a._cur_target_x, a._cur_target_y)
+
+  for move, time_sec in dirs:
+    a.hero.move(move)
+    time.sleep(time_sec)
 
 
 
