a9fdacf91a54e6924946f5e91627db3387161db2
pavlop <poliakov.pavel@gmail.com>
2019-08-15 21:36:51 -0700

    tab -> spaces

--- src/menu_navigator.py
+++ src/menu_navigator.py
@@ -3,25 +3,25 @@ import pyautogui
 
 
 def move_to_map():
-	pyautogui.keyDown('d')
-	time.sleep(0.5)
-	pyautogui.keyUp('d')
-	pyautogui.hotkey('f')
+  pyautogui.keyDown('d')
+  time.sleep(0.5)
+  pyautogui.keyUp('d')
+  pyautogui.hotkey('f')
 
 
 def select_level():
-	move_to_map()
-	pyautogui.hotkey('up')
-	pyautogui.hotkey('up')
-	pyautogui.hotkey('right')
+  move_to_map()
+  pyautogui.hotkey('up')
+  pyautogui.hotkey('up')
+  pyautogui.hotkey('right')
 
 
 def vote_reset():
-	pyautogui.hotkey('escape')
-	pyautogui.hotkey('down')
-	pyautogui.hotkey('down')
-	pyautogui.hotkey('enter')
-	time.sleep(2.8)
+  pyautogui.hotkey('escape')
+  pyautogui.hotkey('down')
+  pyautogui.hotkey('down')
+  pyautogui.hotkey('enter')
+  time.sleep(2.8)
 
 
 vote_reset()
